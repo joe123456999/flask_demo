8955271c7b00692e0b15e79eed7923626674f0b9
joe huang <joehuang@joede-MacBook-Pro.local>
2018-09-15 18:06:41 +0800

    add hello

--- line_bot.py
+++ line_bot.py
@@ -35,7 +35,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    reply = 'your message %s' % event.message.text
+    if event.message.text == 'hello':
+        reply = 'hello Joe'
+    else:
+        reply = 'your message %s' % event.message.text
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply))
